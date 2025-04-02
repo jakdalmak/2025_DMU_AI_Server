440dad466ee8c98a86a264c447962bd7db446dee
jakdalmak7961 <jakdalmak7961@gmail.com>
2025-04-02 17:50:38 +0900

    firstCommit : 기본적인 의존성 추가

---
+++ src/main/java/com/DMU2025_AI/WOW_Raid_Advicer/WowRaidAdvicerApplication.java
@@ -0,0 +1,13 @@
+package com.DMU2025_AI.WOW_Raid_Advicer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowRaidAdvicerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowRaidAdvicerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DMU2025_AI/WOW_Raid_Advicer/WowRaidAdvicerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DMU2025_AI.WOW_Raid_Advicer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowRaidAdvicerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
